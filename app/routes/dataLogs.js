2c7490cb716d0d09ae202cce517e6ffb35a845ee
unknown <abdul.ghofar@suitmedia.com>
2022-10-05 21:28:48 +0700

    setup user endpoint

--- app/routes/dataLogs.js
+++ app/routes/dataLogs.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const c_dataLogs = require('../controller/c_dataLogs');
+const express = require('express')
+const c_dataLogs = require('../controller/c_dataLogs')
 
-const router = express.Router();
+const router = express.Router()
 
-router.get('/', c_dataLogs.getAllData);
-router.post('/', c_dataLogs.insertJSON);
+router.get('/', c_dataLogs.getAllData)
+router.post('/', c_dataLogs.insertJSON)
 
 module.exports = router;
