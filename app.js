9482c40899b39f0504552a0259f87600ceeeaabf
unknown <abdul.ghofar@suitmedia.com>
2022-10-05 12:49:00 +0700

    add datalogs post

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 require("dotenv").config()
-require('./app/config/mongo_connect');
+require('./app/config/mongo_connect')
+bodyParser = require('body-parser')
 
 const express = require('express')
 const r_dataLogs = require('./app/routes/dataLogs')
 
 const app = express()
 const router = express.Router()
+app.use(bodyParser.json());
 
 app.use('/data_logs', r_dataLogs)
 
