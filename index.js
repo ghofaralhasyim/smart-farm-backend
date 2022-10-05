184f1fc17fb9ad1a9869f9c5a1a0ca0f3220888b
unknown <abdul.ghofar@suitmedia.com>
2022-10-05 13:51:55 +0700

    test

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const express = require('express')
 const r_dataLogs = require('./app/routes/dataLogs')
 
 const app = express()
-const router = express.Router()
 app.use(bodyParser.json());
 
 app.use('/data_logs', r_dataLogs)
 
-const server = app.listen(process.env.PORT || 3000, function () {
+const PORT = process.env.PORT || 3000
+app.listen(PORT, function () {
     console.log("Server is running on port 3000")
 });
 
